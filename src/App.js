1671806c2a91966b36d9af803e4db540511a5c5e
Ken Lin <xil307@ucsd.edu>
2019-05-27 16:42:58 -0700

    withQuery

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button, Form, Row, Col} from 'react-bootstrap'
+import withQuery from 'with-query'
 
 
 var sentence = "I hate this resturant, Never come here"
@@ -26,9 +27,13 @@ class Word extends React.Component{
 
 class App extends React.Component {
   handleClick(e){
-    fetch("http://127.0.0.1:5000/explain", { 
-      model_id: 1, 
-      sentence: "I hate this resturant, Never come here",
+    // let url = new URL('http://127.0.0.1:5000/explain')
+    // let params = new URLSearchParams([['model_id', 1], ['sentence', sentence]])
+    // url.search = params
+    fetch(withQuery('http://127.0.0.1:5000/explain', {
+      model_id: 1,
+      sentence: sentence
+    }), { 
       mode: 'no-cors'
     })
     .then(data => console.log(data))
@@ -39,18 +44,21 @@ class App extends React.Component {
     return (
       <div className="App">
         <Row>
-        <Col md={6}>
+
+        <Col sm={6} md={6} lg={6}>
           <Form.Group controlId="exampleForm.ControlTextarea1">
             <Form.Label>Input Sentence</Form.Label>
             <Form.Control as="textarea" rows="3" />
           </Form.Group>
           <Button size='sm' onClick={() => this.handleClick()}> Click </Button>
         </Col>
+
+        <Col sm={6} md={6}>
           <p> {sentence.split(" ").map((w, i) => <Word key={i} word={w} sign="positive"/> )}</p>
-        <Col md={6}>
         </Col>
-        </Row>
-      </div>
+
+      </Row>
+    </div>
     );
   }
 }
